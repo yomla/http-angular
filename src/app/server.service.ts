9c1bca2ffaf783215ef493e190c08bcd7b95fdd6
Mladen <djoml8bre@gmail.com>
2018-01-25 19:52:41 +0100

    Commit-6 Transforming Responses with Observable Operators(map) and centralizing it in service

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
+import { Http, Headers, Response } from '@angular/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class ServerService {
@@ -20,6 +21,12 @@ export class ServerService {
   }
 
   getServers() {
-    return this.http.get('https://udemy-ng-http-3fd16.firebaseio.com/data.json');
+    return this.http.get('https://udemy-ng-http-3fd16.firebaseio.com/data.json')
+    .map(
+      (response: Response) => {
+        const data = response.json();
+        return data;
+      }
+    );
   }
 }
