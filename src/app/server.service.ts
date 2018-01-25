0acc21fc7dce5733a87e62294fb6dc68514bfee7
Mladen <djoml8bre@gmail.com>
2018-01-25 20:18:33 +0100

    Commit-8 Catching Http Errors

--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
 import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ServerService {
@@ -21,7 +22,7 @@ export class ServerService {
   }
 
   getServers() {
-    return this.http.get('https://udemy-ng-http-3fd16.firebaseio.com/data.json')
+    return this.http.get('https://udemy-ng-http-3fd16.firebaseio.com/data')
     .map(
       (response: Response) => {
         const data = response.json();
@@ -29,6 +30,11 @@ export class ServerService {
           server.name = 'FETCHED_' + server.name;
         }
         return data;
+      }
+    )
+    .catch(
+      (error: Response) => {
+        return Observable.throw('Something went wrong!');
       }
     );
   }
